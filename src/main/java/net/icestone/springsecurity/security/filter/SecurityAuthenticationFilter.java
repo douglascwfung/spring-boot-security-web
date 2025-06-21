b1c2f13fabc0f7e85457553711cfb0fd1eb295ad
DouglasFung <icestone.net@gmail.com>
2025-06-21 12:08:04 +0800

    simple-jwt

--- src/main/java/net/icestone/springsecurity/security/filter/SecurityAuthenticationFilter.java
+++ src/main/java/net/icestone/springsecurity/security/filter/SecurityAuthenticationFilter.java
@@ -4,7 +4,7 @@ import net.icestone.springsecurity.common.AuthConstants;
 import net.icestone.springsecurity.security.authentication.UserAuthentication;
 import net.icestone.springsecurity.security.exception.TokenAuthenticationException;
 import net.icestone.springsecurity.security.user.AuthUser;
-import net.icestone.springsecurity.security.user.AuthUserCache;
+import net.icestone.springsecurity.service.jwt.JwtService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -18,10 +18,10 @@ import org.springframework.web.filter.OncePerRequestFilter;
 @Component
 public class SecurityAuthenticationFilter extends OncePerRequestFilter {
 
-  private final AuthUserCache authUserCache;
+  private final JwtService jwtService;
 
-  public SecurityAuthenticationFilter(AuthUserCache authUserCache) {
-    this.authUserCache = authUserCache;
+  public SecurityAuthenticationFilter(JwtService jwtService) {
+    this.jwtService = jwtService;
   }
 
   @Override
@@ -37,10 +37,8 @@ public class SecurityAuthenticationFilter extends OncePerRequestFilter {
       return;
     }
 
-    AuthUser authUser =
-        authUserCache
-            .getByToken(authenticationHeader)
-            .orElseThrow(() -> new TokenAuthenticationException("Token is not valid"));
+    String jwtToken = stripBearerPrefix(authenticationHeader);
+    AuthUser authUser = jwtService.resolveJwtToken(jwtToken);
 
     UserAuthentication authentication = new UserAuthentication(authUser);
 
@@ -49,5 +47,14 @@ public class SecurityAuthenticationFilter extends OncePerRequestFilter {
     SecurityContextHolder.setContext(securityContext);
 
     filterChain.doFilter(request, response);
+  }
+
+  String stripBearerPrefix(String token) {
+
+    if (!token.startsWith("Bearer")) {
+      throw new TokenAuthenticationException("Unsupported authentication scheme");
+    }
+
+    return token.substring(7);
   }
 }
--- src/main/java/net/icestone/springsecurity/service/AuthService.java
+++ src/main/java/net/icestone/springsecurity/service/AuthService.java
@@ -6,7 +6,8 @@ import net.icestone.springsecurity.security.dto.LoginDto;
 import net.icestone.springsecurity.security.dto.TokenDto;
 import net.icestone.springsecurity.security.exception.ApplicationAuthenticationException;
 import net.icestone.springsecurity.security.user.AuthUser;
-import net.icestone.springsecurity.security.user.AuthUserCache;
+import net.icestone.springsecurity.service.jwt.JwtService;
+
 import java.util.UUID;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -14,39 +15,43 @@ import org.springframework.stereotype.Service;
 @Service
 public class AuthService {
 
-  private final AuthUserCache authUserCache;
 
-  private final UserService userService;
+ private final UserService userService;
 
-  private final PasswordEncoder passwordEncoder;
 
-  public AuthService(
-      AuthUserCache authUserCache, UserService userService, PasswordEncoder passwordEncoder) {
-    this.authUserCache = authUserCache;
-    this.userService = userService;
-    this.passwordEncoder = passwordEncoder;
-  }
+ private final PasswordEncoder passwordEncoder;
 
-  public TokenDto login(LoginDto loginDto) {
 
-    UserResponseWithCredentials userCredentials =
-        userService.getUserCredentialsByUsername(loginDto.username());
+ private final JwtService jwtService;
 
-    if (!passwordEncoder.matches(loginDto.password(), userCredentials.passwordHash())) {
-      throw new ApplicationAuthenticationException("Password is incorrect");
-    }
 
-    String token = UUID.randomUUID().toString();
-    UserResponse userResponse = userCredentials.userResponse();
-    AuthUser authUser = new AuthUser(userResponse.id(), userResponse.roles());
+ public AuthService(
+     UserService userService, PasswordEncoder passwordEncoder, JwtService jwtService) {
+   this.userService = userService;
+   this.passwordEncoder = passwordEncoder;
+   this.jwtService = jwtService;
+ }
 
-    authUserCache.login(token, authUser);
 
-    return new TokenDto(token);
-  }
+ public TokenDto login(LoginDto loginDto) {
 
-  public void logout(String token) {
 
-    authUserCache.logout(token);
-  }
+   UserResponseWithCredentials userCredentials =
+       userService.getUserCredentialsByUsername(loginDto.username());
+
+
+   if (!passwordEncoder.matches(loginDto.password(), userCredentials.passwordHash())) {
+     throw new ApplicationAuthenticationException("Password is incorrect");
+   }
+
+
+   UserResponse userResponse = userCredentials.userResponse();
+   AuthUser authUser = new AuthUser(userResponse.id(), userResponse.roles());
+
+
+   String jwtToken = jwtService.createJwtToken(authUser);
+
+
+   return new TokenDto(jwtToken);
+ }
 }
